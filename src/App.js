6f722d45145dd15bd01642384546e6a1988d794a
JorgeLuisPenaranda <jorge.penaranda.arandia@gmail.com>
2022-07-26 16:29:58 -0700

    Stage test 03

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>CIDR Calculator Staging New env</div>
+        <div>CIDR Calculator Staging Test in progres</div>
       </header>
       <IPv4Addr />
       <footer>

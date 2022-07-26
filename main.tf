37659fe888ea4c47dc451da06a8f7083f8a9b978
JorgeLuisPenaranda <jorge.penaranda.arandia@gmail.com>
2022-07-26 14:32:22 -0700

    Testing GH Actions CI 12

--- main.tf
+++ main.tf
@@ -39,8 +39,40 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
+  key_name = jpenaranda
+  vpc_security_group_ids = [ aws_security_group.tt2sg.id ]
 
   tags = {
     Name = "HelloWorld"
+  }
+}
+
+resource "aws_security_group" "tt2sg" {
+  name        = "tt2sg"
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
   }
 }
